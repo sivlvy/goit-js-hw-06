28aa672a7e3c2496770c8331d02756f27a8fca74
sivlvy <vladislavsidenko@ukr.net>
2023-10-21 11:17:35 +0300

    final

--- js/task-08.js
+++ js/task-08.js
@@ -1,8 +1,20 @@
-const form = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 
-form.addEventListener('click', handleClick);
+form.addEventListener("submit", handleClick);
 
 function handleClick(event) {
-event.preventDefault();
-if()
+	event.preventDefault();
+	const loginInput = form.elements.email;
+	const passInput = form.elements.password;
+
+	if (!loginInput.value || !passInput.value) {
+		alert("Будь-ласка, заповніть поля");
+	} else {
+		const formData = {
+			email: loginInput.value,
+			password: passInput.value,
+		};
+		console.log(formData);
+	}
+	form.reset();
 }
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+const body = document.body;
+
+btn.addEventListener("click", handleClick);
+
+function handleClick() {
+	const randomColor = getRandomHexColor();
+	body.style.backgroundColor = randomColor;
+	span.textContent = randomColor.value;
 }
